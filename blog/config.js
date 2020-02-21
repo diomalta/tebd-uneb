80bfbb328821319e76c3f9d196126f846de5f1ab
Diego Santos <diego.santos@cubos.io.com>
2020-02-21 13:43:21 -0300

    att configs

--- blog/config.js
+++ blog/config.js
@@ -1,24 +1,24 @@
 module.exports = {
   // the name of your website
-  title: 'tebd',
+  title: 'TEBD-UNEB',
   // the description of the website (eg. what shows on Google)
-  description: "tebd's blog",
+  description: "Trabalho da Universidade do Estado da Bahia",
   // a short bio shown at the bottom of your blog posts
   // It should complete the sentence: Written by tebd ...
-  shortBio: '',
+  shortBio: 'Projeto da matéria Tópicos Especiais em Banco de dados sendo auxiliado pelo docente Eduardo Jorge.',
   // a longer bio showing on the landing page of the blog
-  bio: ``,
-  author: 'tebd',
+  bio: `Blog feito com intuito de mostrar todos passos para a execução do projeto da matéria Tópicos Especiais em Banco de dados sendo auxiliado pelo docente Eduardo Jorge.`,
+  author: 'TeamJS',
   githubUrl: 'https://github.com/diomalta/tebd-uneb/tebd',
   // replace this by the url where your website will be published
   siteUrl: 'https://tebd-uneb.netlify.com/',
   social: {
     // leave the social media you do not want to appear as empty strings
-    twitter: '',
-    medium: '@gobystrok',
-    facebook: '',
-    github: '',
-    linkedin: '',
-    instagram: '',
+    twitter: '@TeamJS',
+    medium: '@TeamJS',
+    facebook: '@TeamJS',
+    github: '@TeamJS',
+    linkedin: '@TeamJS',
+    instagram: '@TeamJS',
   },
 }
