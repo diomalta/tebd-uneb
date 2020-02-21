b234fc67f341b376a19aff1745057a65d736c7e6
Diego Santos <diego.santos@cubos.io.com>
2020-02-21 11:58:26 -0300

    fix folder

---
+++ blog/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // the name of your website
+  title: 'tebd',
+  // the description of the website (eg. what shows on Google)
+  description: "tebd's blog",
+  // a short bio shown at the bottom of your blog posts
+  // It should complete the sentence: Written by tebd ...
+  shortBio: '',
+  // a longer bio showing on the landing page of the blog
+  bio: ``,
+  author: 'tebd',
+  githubUrl: 'https://github.com/diomalta/tebd-uneb/tebd-blog',
+  // replace this by the url where your website will be published
+  siteUrl: 'https://tebd-uneb.netlify.com/',
+  social: {
+    // leave the social media you do not want to appear as empty strings
+    twitter: '',
+    medium: '@gobystrok',
+    facebook: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+  },
+}
